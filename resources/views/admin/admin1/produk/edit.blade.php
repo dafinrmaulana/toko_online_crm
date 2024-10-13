750b3f91153a1c4def9018e4cc473fa3bf858f42
Dafi <dafinmaulana18@gmail.com>
2024-10-13 21:26:40 +0700

    Revert "fix: add validation message & change default value select to none"
    This reverts commit 335a39162c20178b71016010a5e359f241d11ec0.

--- resources/views/admin/admin1/produk/edit.blade.php
+++ resources/views/admin/admin1/produk/edit.blade.php
@@ -61,7 +61,7 @@
                                         @error('kondisi')
                                             is-invalid
                                         @enderror">
-                                            <option selected disabled value="">Pilih Kondisi Barang</option>
+                                            <option selected>Pilih Kondisi Barang</option>
                                             <option value="baru">Baru</option>
                                             <option value="bekas">Bekas</option>
                                         </select>
@@ -76,7 +76,7 @@
                                         @error('kategori')
                                             is-invalid
                                         @enderror">
-                                            <option selected disabled value="">Pilih Kategori Barang</option>
+                                            <option selected>Pilih Kategori Barang</option>
                                             <option value="cctv">cctv</option>
                                             <option value="notebook">notebook</option>
                                             <option value="pc">pc</option>
